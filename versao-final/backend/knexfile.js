3bc5ee1145a6612a7153256ab03d9f0a80e58d31
Lucas <lucasatletico@hotmail.com>
2020-08-20 16:46:08 -0300

    criando arquivos middleware, e api user

--- versao-final/backend/knexfile.js
+++ versao-final/backend/knexfile.js
@@ -1,13 +1,17 @@
 const { db } = require('./.env')
 
 module.exports = {
-	client: 'postgresql',
-	connection: db,
-	pool: {
-		min: 2,
-		max: 10
-	},
-	migrations: {
-		tableName: 'knex_migrations'
-	}
+    client: 'postgresql',
+    connection: {
+        database: 'knowledge_final',
+        user: 'postgres',
+        password: '123456'
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
 };
